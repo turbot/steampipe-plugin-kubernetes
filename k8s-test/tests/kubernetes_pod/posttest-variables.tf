e0efc5f634deb66978fb7edb7a572ea483475ace
LalitTurbot <lalit@turbot.com>
2021-04-20 23:44:44 +0530

    Add privilaged pod to test

--- k8s-test/tests/kubernetes_pod/posttest-variables.tf
+++ k8s-test/tests/kubernetes_pod/posttest-variables.tf
@@ -1,10 +1,17 @@
-locals {
-  filepath = "${path.cwd}/naked-pod.yml"
-}
-
-resource "null_resource" "named_test_resource" {
+resource "null_resource" "naked-pod" {
   provisioner "local-exec" {
-    command = "kubectl delete -f ${local.filepath}"
+    command = "kubectl delete -f ${path.cwd}/naked-pod.yml"
   }
 }
 
+resource "null_resource" "privileged-pod" {
+  provisioner "local-exec" {
+    command = "kubectl delete -f ${path.cwd}/privileged-pod.yml"
+  }
+}
+
+resource "null_resource" "pull-backoff" {
+  provisioner "local-exec" {
+    command = "kubectl delete -f ${path.cwd}/pull-backoff.yml"
+  }
+}
--- k8s-test/tests/kubernetes_pod/variables.tf
+++ k8s-test/tests/kubernetes_pod/variables.tf
@@ -1,14 +1,26 @@
-locals {
-  filepath = "${path.cwd}/naked-pod.yml"
-}
+# locals {
+#   filepath = "${path.cwd}/naked-pod.yml"
+# }
 
-output "filepath" {
-  value = local.filepath
-}
+# output "filepath" {
+#   value = local.filepath
+# }
 
-resource "null_resource" "named_test_resource" {
+resource "null_resource" "naked-pod" {
   provisioner "local-exec" {
-    command = "kubectl create -f ${local.filepath}"
+    command = "kubectl create -f ${path.cwd}/naked-pod.yml"
+  }
+}
+
+resource "null_resource" "privileged-pod" {
+  provisioner "local-exec" {
+    command = "kubectl create -f ${path.cwd}/privileged-pod.yml"
+  }
+}
+
+resource "null_resource" "pull-backoff" {
+  provisioner "local-exec" {
+    command = "kubectl create -f ${path.cwd}/pull-backoff.yml"
   }
 }
 
