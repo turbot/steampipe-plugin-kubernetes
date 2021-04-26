830ef43f207b145a3cedd892613bec3c67fc2400
LalitTurbot <lalit@turbot.com>
2021-04-26 17:35:10 +0530

    Add kubernetes_replication_controller table

--- kubernetes/plugin.go
+++ kubernetes/plugin.go
@@ -45,6 +45,7 @@ func Plugin(ctx context.Context) *plugin.Plugin {
 			"kubernetes_pod":                     tableKubernetesPod(ctx),
 			"kubernetes_pod_security_policy":     tableKubernetesPodSecurityPolicy(ctx),
 			"kubernetes_replicaset":              tableKubernetesReplicaSet(ctx),
+			"kubernetes_replication_controller":  tableKubernetesReplicaController(ctx),
 			"kubernetes_role":                    tableKubernetesRole(ctx),
 			"kubernetes_role_binding":            tableKubernetesRoleBinding(ctx),
 			"kubernetes_secret":                  tableKubernetesSecret(ctx),
