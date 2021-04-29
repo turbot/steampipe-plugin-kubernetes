3ae10b38fa31502488c1f6e4f6076a6179a39fa4
LalitTurbot <lalit@turbot.com>
2021-04-29 15:34:46 +0530

    Add minimize the admission of root containers interation test

---
+++ k8s-test/tests/root_containers/variables.tf
@@ -0,0 +1,32 @@
+# deploy rootContainersFAILED.yaml
+resource "null_resource" "root_containers_failed" {
+  provisioner "local-exec" {
+    command = "kubectl create -f ${path.cwd}/rootContainersFAILED.yaml"
+  }
+}
+
+# deploy rootContainersPASSED.yaml
+resource "null_resource" "root_containers_passed" {
+  provisioner "local-exec" {
+    command = "kubectl create -f ${path.cwd}/rootContainersPASSED.yaml"
+  }
+}
+
+resource "null_resource" "delay" {
+  provisioner "local-exec" {
+    command = "sleep 100"
+  }
+}
+
+
+# Delay in order to get te resource creation complete
+resource "null_resource" "get_pods" {
+  depends_on = [
+    null_resource.delay
+  ]
+  provisioner "local-exec" {
+    command = "kubectl get pods"
+  }
+}
+
+
