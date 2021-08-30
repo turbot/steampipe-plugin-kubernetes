89ee4639bf130fc745b1d9237f0b8661c8b10b67
bigdatasourav <bigdatasourav@gmail.com>
2021-08-30 17:12:03 +0530

    update

--- k8s-test/tests/kubernetes_resource_quota/posttest-variables.tf
+++ k8s-test/tests/kubernetes_resource_quota/posttest-variables.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "delete_quota" {
   provisioner "local-exec" {
-    command = "kubectl delete -f ${path.cwd}/quota.yaml"
+    command = "kubectl delete -f ${path.cwd}/resource_quota.yaml"
   }
 }
 
