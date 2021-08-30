58bdd671bcf05d3c42716c53abc165ec14bb8362
bigdatasourav <bigdatasourav@gmail.com>
2021-08-30 17:12:36 +0530

    update

--- k8s-test/tests/kubernetes_limit_range/posttest-variables.tf
+++ k8s-test/tests/kubernetes_limit_range/posttest-variables.tf
@@ -1,6 +1,6 @@
 resource "null_resource" "delete_limit" {
   provisioner "local-exec" {
-    command = "kubectl delete -f ${path.cwd}/limit.yaml"
+    command = "kubectl delete -f ${path.cwd}/limit_range.yaml"
   }
 }
 
