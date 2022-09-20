26f37acd3d1160cead1ec2071514fbfe8d8811c6
sourav chakraborty <sourav@turbot.com>
2022-09-20 12:15:07 +0800

    Full dynamic

--- go.mod
+++ go.mod
@@ -32,7 +32,6 @@ require (
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/emicklei/go-restful/v3 v3.8.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
-	github.com/form3tech-oss/jwt-go v3.2.3+incompatible // indirect
 	github.com/gertd/go-pluralize v0.2.1 // indirect
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
@@ -45,13 +44,11 @@ require (
 	github.com/golang/glog v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/gnostic v0.5.7-v3refs // indirect
-	github.com/google/go-cmp v0.5.8 // indirect
 	github.com/google/gofuzz v1.1.0 // indirect
-	github.com/googleapis/gnostic v0.5.5 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.0 // indirect
 	github.com/hashicorp/go-hclog v1.2.0 // indirect
 	github.com/hashicorp/go-plugin v1.4.4 // indirect
-	github.com/hashicorp/go-version v1.5.0 // indirect
+	github.com/hashicorp/go-version v1.6.0 // indirect
 	github.com/hashicorp/hcl/v2 v2.12.0 // indirect
 	github.com/hashicorp/yamux v0.0.0-20181012175058-2f1d1f20f75d // indirect
 	github.com/iancoleman/strcase v0.2.0 // indirect
@@ -98,12 +95,11 @@ require (
 	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
-	google.golang.org/grpc v1.47.0 // indirect
+	google.golang.org/grpc v1.48.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/apiserver v0.25.1 // indirect
 	k8s.io/klog/v2 v2.70.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20220803162953-67bda5d908f1 // indirect
 	k8s.io/utils v0.0.0-20220728103510-ee6ede2d64ed // indirect
--- kubernetes/plugin.go
+++ kubernetes/plugin.go
@@ -9,8 +9,11 @@ package kubernetes
 import (
 	"context"
 
+	"github.com/turbot/steampipe-plugin-sdk/v3/connection"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v3/plugin/transform"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 const pluginName = "steampipe-plugin-kubernetes"
@@ -68,15 +71,16 @@ func pluginTableDefinitions(ctx context.Context, p *plugin.Plugin) (map[string]*
 		"kubernetes_crd":                     tableKubernetesCRD(ctx),
 	}
 
-	// Search for metrics to create as tables
+	// Search for CRs to create as tables
 	kubernetesTables := []string{}
-	config := GetConfig(p.Connection)
-	if config.CustomResources != nil && len(*config.CustomResources) > 0 {
-		for _, tableName := range *config.CustomResources {
-			pluginTableName := "kubernetes_crd_" + tableName
-			if _, ok := tables[pluginTableName]; !ok {
-				kubernetesTables = append(kubernetesTables, tableName)
-			}
+	crds, err := listK8sDynamicCRDs(ctx, p.ConnectionManager, p.Connection)
+	if err != nil {
+		return nil, err
+	}
+	for _, crd := range crds {
+		pluginTableName := "kubernetes_crd_" + crd.Spec.Names.Plural
+		if _, ok := tables[pluginTableName]; !ok {
+			kubernetesTables = append(kubernetesTables, crd.Spec.Names.Plural)
 		}
 	}
 
@@ -94,4 +98,41 @@ func pluginTableDefinitions(ctx context.Context, p *plugin.Plugin) (map[string]*
 	}
 
 	return tables, nil
+}
+
+func listK8sDynamicCRDs(ctx context.Context, cm *connection.Manager, c *plugin.Connection) ([]v1.CustomResourceDefinition, error) {
+	logger := plugin.Logger(ctx)
+	logger.Trace("listK8sDynamicCRDs")
+
+	clientset, err := GetNewClientCRDRaw(ctx, cm, c)
+	if err != nil {
+		return nil, err
+	}
+
+	input := metav1.ListOptions{
+		Limit: 500,
+	}
+
+	crds := []v1.CustomResourceDefinition{}
+
+	pageLeft := true
+	for pageLeft {
+		response, err := clientset.ApiextensionsV1().CustomResourceDefinitions().List(ctx, input)
+		if err != nil {
+			logger.Error("listK8sDynamicCRDs", "list_err", err)
+			return nil, err
+		}
+
+		if response.GetContinue() != "" {
+			input.Continue = response.Continue
+		} else {
+			pageLeft = false
+		}
+
+		for _, crd := range response.Items {
+			crds = append(crds, crd)
+		}
+	}
+
+	return crds, nil
 }
