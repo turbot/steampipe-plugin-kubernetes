b0d5ff9523dd80d1dd5b0393900cd54824e2ecb1
Subhajit Mondal <subhajit@turbot.com>
2023-04-05 16:24:36 +0530

    Remove unused function

--- kubernetes/utils.go
+++ kubernetes/utils.go
@@ -581,58 +581,6 @@ func mergeTags(labels map[string]string, annotations map[string]string) map[stri
 	return tags
 }
 
-type filePath struct {
-	Path string
-}
-
-// Remove when all the manifest tables are merged with the existing one
-func listKubernetesManifestFiles(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	// #1 - Path via qual
-
-	// If the path was requested through qualifier then match it exactly. Globs
-	// are not supported in this context since the output value for the column
-	// will never match the requested value.
-	quals := d.EqualsQuals
-	if quals["manifest_file_path"] != nil {
-		d.StreamListItem(ctx, filePath{Path: quals["manifest_file_path"].GetStringValue()})
-		return nil, nil
-	}
-
-	// #2 - paths in config
-
-	// Glob paths in config
-	// Fail if no paths are specified
-	k8sConfig := GetConfig(d.Connection)
-	if k8sConfig.ManifestFilePaths == nil {
-		return nil, errors.New("manifest_file_path must be configured")
-	}
-
-	// Gather file path matches for the glob
-	var matches []string
-	paths := k8sConfig.ManifestFilePaths
-	for _, i := range paths {
-
-		// List the files in the given source directory
-		files, err := d.GetSourceFiles(i)
-		if err != nil {
-			return nil, err
-		}
-		matches = append(matches, files...)
-	}
-
-	// Sanitize the matches to ignore the directories
-	for _, i := range matches {
-
-		// Ignore directories
-		if filehelpers.DirectoryExists(i) {
-			continue
-		}
-		d.StreamListItem(ctx, filePath{Path: i})
-	}
-
-	return nil, nil
-}
-
 // Check whether the config file is configured correctly.
 // The plugin can list both live resources and the resources from the manifest file.
 // To avoid the conflict, a connection can only contains either config_path / config_paths to define the kube config files, or,
