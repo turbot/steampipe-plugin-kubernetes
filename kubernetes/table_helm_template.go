1b101bc0d01ee5eca6d1374aa6302ec47e9fb1bb
Cody Bruno <cody@turbot.com>
2026-07-28 16:59:47 -0400

    Recompile plugin with steampipe-plugin-sdk v6.0.0

--- kubernetes/table_helm_template.go
+++ kubernetes/table_helm_template.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"path"
 
-	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v6/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v6/plugin"
 )
 
 //// TABLE DEFINITION
--- kubernetes/table_helm_value.go
+++ kubernetes/table_helm_value.go
@@ -5,9 +5,9 @@ import (
 	"os"
 	"path"
 
-	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
-	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
+	"github.com/turbot/steampipe-plugin-sdk/v6/grpc/proto"
+	"github.com/turbot/steampipe-plugin-sdk/v6/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v6/plugin/transform"
 	"gopkg.in/yaml.v3"
 )
 
